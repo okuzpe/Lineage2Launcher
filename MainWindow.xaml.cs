a9abb446a74ee211c3bd73058e48e8790679f915
okuzpe <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>
2025-12-11 01:17:36 +0100

    feat(ui): add MainWindow launcher layout (3 columns)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using Lineage2Launcher.ViewModels;
+
+namespace Lineage2Launcher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
